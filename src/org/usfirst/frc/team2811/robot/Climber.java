d12bc57a7f5c3c1818a334fc3434209927ecf792
Stormbots Driverstation <tekdemo+driverstation@gmail.com>
2018-02-05 18:56:22 -0800

    removed private FB function

--- src/org/usfirst/frc/team2811/robot/Climber.java
+++ src/org/usfirst/frc/team2811/robot/Climber.java
@@ -19,30 +19,12 @@ public class Climber extends RobotModule {
 	public void setVelocity(double velocity) {
 		TarVel = velocity;
 	}
-	
-	double FB(double V, double P, double K) {
-		double vx = 0.0;
-		if(V>P) {
-			vx = K*Math.sqrt(Math.abs(V-P));
-		}
-		else {
-			vx = -K*Math.sqrt(Math.abs(P-V));
-		}
-		if(vx>1){
-			vx=1;
-		}
-		if(vx<-1){
-			vx = -1;
-		}
-		return vx;
-	}
-	
+		
 	void update(Joystick driver1,Joystick driver2, Joystick stick) {
 		mtr1.set(stick.getY());
 		mtr2.set(stick.getY());
 		//? SmartDashboard.putNumber("velocity", TarVel);
 		//SmartDashboard.putNumber("position", TarPos);
-		
 		
 	}
 }
