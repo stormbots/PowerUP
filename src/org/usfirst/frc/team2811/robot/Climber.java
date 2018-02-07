9a4bcf6a9bd772ecae5235437b30eac8d2cf5622
Stormbots Driverstation <tekdemo+driverstation@gmail.com>
2018-02-06 20:14:41 -0800

    Added code for second motor (commented out)

--- src/org/usfirst/frc/team2811/robot/Climber.java
+++ src/org/usfirst/frc/team2811/robot/Climber.java
@@ -8,15 +8,19 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class Climber extends RobotModule {
 	
 	WPI_TalonSRX mtr1 = new WPI_TalonSRX(12);
+	//WPI_TalonSRX mtr2 = new WPI_TalonSRX(??);
 		
 	void update(Joystick driver1,Joystick driver2, Joystick stick) {
 		if(stick.getRawButton(5)) {
-		mtr1.set(ControlMode.PercentOutput, stick.getY());
+		mtr1.set(ControlMode.PercentOutput, stick.getY()); //replace getY with fixed value on robot
+		//mtr2.set(ControlMode.PercentOutput, stick.getY());
 		}
 		else {
 			mtr1.set(ControlMode.PercentOutput, 0);
+			//mtr2.set(ControlMode.PercentOutput, 0);
 		}
 		SmartDashboard.putNumber("ClimberCurrent", mtr1.getOutputCurrent());
+		//SmartDashboard.putNumber("ClimberCurrent", mtr2.getOutputCurrent());
 		
 	}
 }
