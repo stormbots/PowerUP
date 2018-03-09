b4fbe286504d119896a56fd0d5836ba24418c66b
Annika <tekdemo+driverstation@gmail.com>
2018-03-08 19:50:03 -0800

    Extra code in elevator and chassis removed
    As Zach wants to put it: "Let bygones be bygones"

--- src/org/usfirst/frc/team2811/robot/Elevator.java
+++ src/org/usfirst/frc/team2811/robot/Elevator.java
@@ -66,10 +66,6 @@ public class Elevator extends RobotModule {
 	public Mode getMode () {
 		return mode;
 	}
-		
-	void init(){
-		//reset();
-	}
 	
 	
 	public void disabledPeriodic(){
@@ -78,112 +74,7 @@ public class Elevator extends RobotModule {
 		
 	}
 	
-	void update(Joystick driver1,Joystick driver2, Joystick functions1) { //Only using functions1
-		
-		double breakpoint = 0.0;
-		
-		//TODO: new joystick needs to be plugged in with slider at zero, or moved, otherwise it reads incorrectly
-		// Fix should be to make sure that the joystick input is -1 before enabling the elevator
 	
-		double stickValue = -functions1.getRawAxis(3);
-
-		if(functions1.getRawButtonPressed(11)) {
-			if(mode == Mode.MANUALPOSITION) {
-				mode = Mode.MANUALVELOCITY;
-			}
-			else if(mode == Mode.MANUALVELOCITY) {
-				mode = Mode.MANUALPOSITION;
-			}
-			else {
-				//this shouldn't happen
-				mode = Mode.MANUALPOSITION;
-			}
-		}
-		
-		if(mode == Mode.HOMING) {
-			if(!LimitSwitch.get()) {
-				eVelocity = 0.1;
-			}
-			else{
-				mode = Mode.MANUALPOSITION;
-				homed = true;
-				reset();
-			}
-		}
-		
-		else if(mode == Mode.MANUALVELOCITY) {
-			
-			eVelocity = stickValue;
-
-			
-			
-		
-		}
-		//Position setlist, currently unused
-		/*else if(mode == Mode.BUTTON) {
-			
-			eVelocity = FB.FB(elevatorPos, -currentPos, 0.02);
-			//Takes an inputed position and adjusts the velocity to get there.
-			//Last value needs to be tested and adjusted.
-			
-		}*/
-		
-		else if(mode == Mode.MANUALPOSITION) {
-			
-			
-			elevatorPos = ( (stickValue-(-1)) / (1-(-1)) * (maxPos-minPos) + minPos ); //Maps controller y-axis to elevator position.
-			
-			//Not applicable to practice robot, not used
-			/*if(elevatorPos < midBP && currentPos > midBP) {
-				breakpoint = midBP;
-			}
-			else if(elevatorPos > midBP && currentPos < midBP) {
-				breakpoint = midBP;
-			}
-			else {
-				breakpoint = elevatorPos;
-				
-			}*/
-			
-			eVelocity = FB.FB(elevatorPos, currentPos, 0.007);			
-			
-		}
-		
-		//Invert motor phase. The Talon command to do this does not seem to work.
-		if(currentPos >= maxPos && eVelocity > 0) { //Keeps elevator from going too high.
-			eVelocity = 0; 
-		}
-		else if(!LimitSwitch.get()) {
-			reset();
-			homed = true;
-			if(eVelocity < 0) {
-				eVelocity = 0;
-			}
-		}
-		else if(functions1.getRawButton(3)) {
-			eVelocity = -0.5;
-		}
-		else if(currentPos <= softLimit && eVelocity < 0) { //Keeps elevator from going too low.
-			eVelocity = 0; 
-		}
-		else {
-		}
-		
-	
-		//Set Motor phase, currently not needed 
-		//eVelocity = -eVelocity;
-		
-		
-		eMotor.set(ControlMode.PercentOutput, eVelocity);
-		SmartDashboard.putNumber("Elevator Current Position", currentPos);
-		SmartDashboard.putNumber("Breakpoint", breakpoint);
-		SmartDashboard.putNumber("Desired Position", elevatorPos);
-		SmartDashboard.putNumber("Voltage", eMotor.getOutputCurrent());
-		SmartDashboard.putNumber("Joystick Position", functions1.getY());
-		SmartDashboard.putNumber("elevatorVelocity", eVelocity);
-		SmartDashboard.putBoolean("LimitSwitch", LimitSwitch.get());
-		
-	}
 		
 	public void reset() {
 		eMotor.setSelectedSensorPosition(0, 0, 20); //First argument is desired position, second is the type of loop? (0 or 1), third is the timeout.
@@ -196,66 +87,12 @@ public class Elevator extends RobotModule {
 
 	}
 		
-/*	void autoInit(RobotLocation robotLocation, TargetLocation targetLocation, SwitchConfig switchConfig, ScaleConfig scaleConfig) { //Elevator only cares about targetLocation
-		eMotor.setSelectedSensorPosition(0, (int) initializePos, 20);
-		eMotor.setSelectedSensorPosition((int) initializePos, 0, 20);// maybe just in case? Shouldn't do anything.
-		
-		if(robotLocation == RobotLocation.CENTER) {
-			autoActiveStep = 3;
-			autoActiveTime = 1;
-		}
-		else {
-			autoActiveStep = 1;
-			autoActiveTime = 5.5;
-		}
-		if(targetLocation == TargetLocation.SCALE) {
-			autoActiveStep = 1;
-			autoActiveTime = 3.5;
-		}
-		
-		elevatorPos=initializePos;
-		if(targetLocation == TargetLocation.SWITCH) {
-			autoPosition = switchPos; // bug fix 201802222021  =scaleHighPos
-		}
-		else if(targetLocation == TargetLocation.SCALE) {
-			autoPosition = scaleHighPos;
-		}
-		else if(targetLocation == TargetLocation.MOVE_ONLY) {
-			autoPosition = switchPos;
-		}
-		else {
-			autoPosition = initializePos;		
-		}
-	}
-	
-	void auto(int stepAuto, double time) {
-		
-		if(time > autoActiveTime && stepAuto == autoActiveStep) {
-			elevatorPos = autoPosition;
-		}
-		
-		//TODO may need to set these motor phases
-		
-		if(prefs.getBoolean("compbot", false)) {
-			//comp bot
-			SmartDashboard.putNumber("ElevatorCurrentPos", eMotor.getSelectedSensorPosition(0));
-			eMotor.set(ControlMode.PercentOutput, FB.FB(elevatorPos, eMotor.getSelectedSensorPosition(0), 0.01));
-		}
-		else {
-			//prac bot
-			eMotor.set(ControlMode.PercentOutput, FB.FB(elevatorPos, -eMotor.getSelectedSensorPosition(0), 0.01));
-			SmartDashboard.putNumber("ElevatorCurrentPos", -eMotor.getSelectedSensorPosition(0));
-		}
-			
-		SmartDashboard.putNumber("ElevatorAutoPos", autoPosition);
-		SmartDashboard.putNumber("Elevatorvelocity", eMotor.getMotorOutputPercent());
-		SmartDashboard.putNumber("ElevatorPos", elevatorPos);
-	}*/
+
 	
 	public void setVel(double velocity) {
 		eVelocity = velocity;
 	}
-	
+		
 	/**
 	 * @param position between -1 and 1
 	 */
@@ -264,17 +101,6 @@ public class Elevator extends RobotModule {
 		elevatorPos = Utilities.lerp(position, -1, 1, 0, maxPos);
 	}
 	
-	public void goHome() {
-		if(!LimitSwitch.get()) {
-			eVelocity = -0.5;
-		}
-		else{
-			mode = Mode.MANUALPOSITION;
-			homed = true;
-			reset();
-		}
-	}
-
 	/**
 	 * 	setPos(ElePosition.SWITCH);
 	 * @param position
