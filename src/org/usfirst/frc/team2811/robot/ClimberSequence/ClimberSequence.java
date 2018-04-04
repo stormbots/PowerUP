be4800e9e2ba78ae91d284685afce6a4c04a3563
Annika <tekdemo+driverstation@gmail.com>
2018-04-03 20:06:07 -0700

    Set up Climber Sequence classes

---
+++ src/org/usfirst/frc/team2811/robot/ClimberSequence/ClimberSequence.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2811.robot.ClimberSequence;
+
+public abstract class ClimberSequence {
+
+	public abstract void run();
+	
+	public abstract void cancel();
+	
+	public abstract void init();
+}
